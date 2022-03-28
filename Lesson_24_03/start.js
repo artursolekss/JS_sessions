9bce73258e6a8822bc454143b8d280953991f2a0
Arturs Olekss <arturs.olekss@gmail.com>
2022-03-28 21:29:58 +0300

    28/03

--- Lesson_24_03/start.js
+++ Lesson_24_03/start.js
@@ -1,33 +1,23 @@
 const http = require('http');
 const mysql = require('mysql');
-const url = require('url');
-const fs = require('fs');
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test"
+})
+
+let data;
+con.connect(function(err) {
+    if (err) return;
+    con.query("SELECT * FROM persons", function(err, result) {
+        if (err) throw err;
+        data = result;
+    });
+});
 
 http.createServer(function(req, res) {
-    // res.writeHead(200, { 'Content-Type': 'text/html' });
-    // res.end('<h1>Hello All</h1>');
-    // const urlpath = url.parse(req.url, true);
-    // const filename = '.' + urlpath.pathname;
-    // fs.readFile(filename, function(error, data) {
-    //     if (error) {
-    //         res.writeHead(404, { 'Content-Type': 'text/html' });
-    //         return res.end('Page not found :(');
-    //     }
-    //     res.writeHead(200, { 'Content-Type': 'text/html' });
-    //     res.write(data);
-    //     res.end();
-    // })
-    // const con = mysql.createConnection({
-    //     host: "localhost",
-    //     user: "root",
-    //     password: ""
-    // })
-    // con.connect(function(err) {
-
-    //     if (err) {
-    //         res.writeHead(404, { 'Content-Type': 'text/html' });
-    //         res.end('Connection is not successful :(');
-    //     }
-    // })
-
-}).listen(80);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(JSON.stringify(data));
+}).listen(8080)
