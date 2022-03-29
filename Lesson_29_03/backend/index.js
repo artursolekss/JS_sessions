4799b71a37d26964fca2ceb2a24e0a8e42152cd9
Arturs Olekss <arturs.olekss@gmail.com>
2022-03-29 19:07:20 +0300

    29_03 (1)

---
+++ Lesson_29_03/backend/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+let mysql = require("mysql");
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test"
+})
+
+connection.connect((error) => {
+    if (error)
+        throw error;
+    console.log("Connected to MYSQl")
+})
+
+app.post("/add-name", (req, res) => {
+    let sql = "INSERT INTO names (name,lastname) VALUES (?,?)";
+    connection.query(sql, [req.body.name, req.body.lastname], (error, results) => {
+        if (error) throw error;
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+    })
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000")
+})
